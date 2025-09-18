696879a1214b663b02c58ab8ecc42bff2b394134
fsampalo <sampalogomezfernando@gmail.com>
2025-09-18 19:47:55 +0200

    Arreglo Deploy

--- app.py
+++ app.py
@@ -523,7 +523,9 @@ def init_db():
         
         db.session.commit()
 
+# Inicializar la base de datos automáticamente en producción
+with app.app_context():
+    init_db()
+
 if __name__ == '__main__':
-    with app.app_context():
-        init_db()
     app.run(debug=True, host='0.0.0.0', port=5000)
